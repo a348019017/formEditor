20ff655904eab8f0535ac6b0b903df092d298933
a348019017 <348019017@example.com>
2023-01-12 18:39:22 +0800

    xxx

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.BaseUrl = "http://10.254.251.224:8021"//服务器"http://10.254.251.224:8021"//王琦林代理"http://192.168.201.191:8021"//芦山现场"http://125.67.201.153:8021"
 //表单等XZP功能使用的api
 //window.BaseUrl2= "http://localhost:8082"
-window.BaseUrl2= "http://10f192b.nat123.fun:53907"
+window.BaseUrl2= "http://www.ponggis.xyz:8084"
 
 window.defaultValues={
   num:"1234567890"
